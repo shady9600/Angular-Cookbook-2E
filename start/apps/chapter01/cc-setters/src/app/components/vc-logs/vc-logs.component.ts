2c0ba3bce994671f4bf941dd9c918c9946cec9c7
Brian Hamlin <bhamlin92@gmail.com>
2024-06-11 08:57:53 -0400

    finish cc-setters

--- start/apps/chapter01/cc-setters/src/app/components/vc-logs/vc-logs.component.ts
+++ start/apps/chapter01/cc-setters/src/app/components/vc-logs/vc-logs.component.ts
@@ -6,5 +6,26 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./vc-logs.component.scss'],
 })
 export class VcLogsComponent {
-  @Input() vName = '';
+  @Input()
+  get vName() {
+    return this._vName;
+  }
+  // runs any logic we need when the input changes
+  set vName(name: string) {
+    if (!name) return;
+    if (!this._vName) {
+      this.logs.push(`initial version is ${name.trim()}`)
+    } else {
+      this.logs.push(`version changed to ${name.trim()}`)
+    }
+    this._vName = name;
+    
+  }
+
+  // modify internal private value; template accesses getter
+  private _vName!: string;
+
+  logs: string[] = [];
+
+
 }
