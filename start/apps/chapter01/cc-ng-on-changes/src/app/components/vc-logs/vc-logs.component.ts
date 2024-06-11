4d1330d3fd84259cccaaefc5ee3f8a3ce4256fe0
Brian Hamlin <bhamlin92@gmail.com>
2024-06-11 09:07:50 -0400

    finish cc-ng-on-changes

--- start/apps/chapter01/cc-ng-on-changes/src/app/components/vc-logs/vc-logs.component.ts
+++ start/apps/chapter01/cc-ng-on-changes/src/app/components/vc-logs/vc-logs.component.ts
@@ -1,10 +1,24 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-vc-logs',
   templateUrl: './vc-logs.component.html',
   styleUrls: ['./vc-logs.component.scss'],
 })
-export class VcLogsComponent {
+export class VcLogsComponent implements OnChanges {
   @Input() vName = '';
+  logs: string[] = [];
+
+  // initially runs before ngOnInit, and on every subsequent change detection
+  ngOnChanges(changes: SimpleChanges): void {
+    // get the changed value for vName input property
+    const { currentValue } = changes['vName'];
+
+    // check if first change or not
+    if (changes['vName'].isFirstChange()) {
+      this.logs.push(`initial version is ${currentValue.trim()}`)
+    } else {
+      this.logs.push(`version updated to ${currentValue.trim()}`)
+    }
+  }
 }
